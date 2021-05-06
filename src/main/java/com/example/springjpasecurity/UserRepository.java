19fb383bad1fac8393757684e64167070b3364e9
odbalapure <odbalapure@gmail.com>
2021-05-06 23:07:34 +0530

    Spring security using JPA

---
+++ src/main/java/com/example/springjpasecurity/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.springjpasecurity;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+	Optional<User> findByUserName(String username);
+	
+}
